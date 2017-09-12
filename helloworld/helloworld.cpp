47b3489b909abfe041a560f9b692109e68c99c54
reillyliu <xnlhs1230@126.com>
2017-09-12 11:20:32 +0800

    修改main，输出Hello C++和push

--- helloworld/helloworld.cpp
+++ helloworld/helloworld.cpp
@@ -1,11 +1,14 @@
 // helloworld.cpp: 定义控制台应用程序的入口点。
 //
 
-#include "stdafx.h"
+#include<iostream>
+using namespace std;
 
 
 int main()
 {
+	cout << "Hello C++";
+	cout << "push";
     return 0;
 }
 
